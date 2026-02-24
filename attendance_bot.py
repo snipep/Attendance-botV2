5a552fa8752107c3b7eb0fb7ba299affcad12e5a
Vishnu Pratap Bisht <vishnupratap24@gmail.com>
2026-02-24 22:46:55 +0530

    Changes based on Self Hosted Runner

--- attendance_bot.py
+++ attendance_bot.py
@@ -54,6 +54,8 @@ def run_attendance():
 
     if is_headless:
         chrome_options.add_argument("--headless=new") # Updated headless flag
+        chrome_options.add_argument("--no-sandbox") 
+        chrome_options.add_argument("--disable-dev-shm-usage") 
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     else:
